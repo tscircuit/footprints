a7dea2911c5d508d72301cc0c1881b990886344e
seveibar <seveibar@gmail.com>
2024-08-18 12:15:50 -0700

    fix type issues

--- src/Axial.tsx
+++ src/Axial.tsx
@@ -1,47 +1,47 @@
-import { mm, mmStr } from "@tscircuit/mm"
+import { mm, mmStr } from "@tscircuit/mm";
 
 interface AxiosProps {
-  spread?: string | number
-  orientation?: "horizontal" | "vertical"
-  hole_diameter?: string | number
-  outer_diameter?: string | number
-  inner_diameter?: string | number
+	spread?: string | number;
+	orientation?: "horizontal" | "vertical";
+	hole_diameter?: string | number;
+	outer_diameter?: string | number;
+	inner_diameter?: string | number;
 }
 
 export const Axial = ({
-  spread = "0.2in",
-  orientation = "horizontal",
-  hole_diameter = "0.8mm",
-  inner_diameter = "1.2mm",
-  outer_diameter = "2mm",
+	spread = "0.2in",
+	orientation = "horizontal",
+	hole_diameter = "0.8mm",
+	inner_diameter = "1.2mm",
+	outer_diameter = "2mm",
 }: AxiosProps) => {
-  const sp = mm(spread)
+	const sp = mm(spread);
 
-  const spx = orientation === "horizontal" ? sp : 0
-  const spy = orientation === "vertical" ? sp : 0
+	const spx = orientation === "horizontal" ? sp : 0;
+	const spy = orientation === "vertical" ? sp : 0;
 
-  const hd = mm(hole_diameter)
-  const id = mm(inner_diameter)
-  const od = mm(outer_diameter)
+	const hd = mm(hole_diameter);
+	const id = mm(inner_diameter);
+	const od = mm(outer_diameter);
 
-  return (
-    <footprint>
-      <platedhole
-        x={-spx / 2}
-        y={0}
-        port_hints={["1", "left"]}
-        hole_diameter={hd}
-        outer_diameter={od}
-        inner_diameter={id}
-      />
-      <platedhole
-        x={spx / 2}
-        y={0}
-        port_hints={["2", "right"]}
-        hole_diameter={hd}
-        outer_diameter={od}
-        inner_diameter={id}
-      />
-    </footprint>
-  )
-}
+	return (
+		<footprint>
+			<platedhole
+				pcbX={-spx / 2}
+				pcbY={0}
+				portHints={["1", "left"]}
+				holeDiameter={hd}
+				outerDiameter={od}
+				shape="circle"
+			/>
+			<platedhole
+				pcbX={spx / 2}
+				pcbY={0}
+				portHints={["2", "right"]}
+				holeDiameter={hd}
+				outerDiameter={od}
+				shape="circle"
+			/>
+		</footprint>
+	);
+};
