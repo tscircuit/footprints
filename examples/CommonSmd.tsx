be8759b34d39d275ece6fc281f42c1750c66139a
seveibar <seveibar@gmail.com>
2024-08-18 12:25:51 -0700

    fix type issue

--- examples/CommonSmd.tsx
+++ examples/CommonSmd.tsx
@@ -1,15 +1,15 @@
-import { Smd0402, Smd0603, Smd0805 } from "../src/CommonSmd"
+import { Smd0402, Smd0603, Smd0805 } from "../src/CommonSmd";
 
 export const CommonSmd1 = () => (
-  <group>
-    <resistor
-      name="0402"
-      resistance="10kohm"
-      pcb_x={0}
-      pcb_y={0}
-      footprint={<Smd0402 />}
-    />
-    <resistor name="0603" resistance="10kohm" footprint={<Smd0603 />} />
-    <resistor name="0805" resistance="10kohm" footprint={<Smd0805 />} />
-  </group>
-)
+	<group>
+		<resistor
+			name="0402"
+			resistance="10kohm"
+			pcbX={0}
+			pcbY={0}
+			footprint={<Smd0402 />}
+		/>
+		<resistor name="0603" resistance="10kohm" footprint={<Smd0603 />} />
+		<resistor name="0805" resistance="10kohm" footprint={<Smd0805 />} />
+	</group>
+);
